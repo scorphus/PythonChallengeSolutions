86d756a868290ba484405441da2be4250678dd90
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-23 16:13:55 -0300

    Add solution to mission 00

---
+++ 00-warmup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/def/0.html
+
+print(1 << 38)  # Faster than `pow(2, 38)`. As fast as `2 ** 38`.
