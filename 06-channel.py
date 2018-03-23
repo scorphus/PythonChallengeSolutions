7608a722c56c0aaf0a974be4a5bf148be0d7d5ba
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-23 16:57:52 -0300

    Add solution to mission 06

---
+++ 06-channel.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/def/channel.html
+# Source mentions zip
+
+import sys
+
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+url = 'http://www.pythonchallenge.com/pc/def/channel.zip'
+zip_content = urlopen(url).read()
+
+with ZipFile(BytesIO(zip_content)) as channel:
+    with channel.open('readme.txt') as readme:
+        for line in readme.readlines():
+            if 'start' in line.decode():
+                current = line.decode().strip().split(' ')[-1]
+                break
+        else:
+            raise RuntimeError('Could not find start')
+    comments = ''
+    while True:
+        print('.', end='', flush=True)
+        comments += channel.getinfo(current + '.txt').comment.decode()
+        try:
+            with channel.open(current + '.txt') as current_fp:
+                riddle = current_fp.read().decode()
+                if 'Next nothing is' not in riddle:
+                    break
+                current = riddle.split(' ')[-1]
+        except KeyError:
+            break
+        except KeyboardInterrupt:
+            sys.exit(0)
+        except Exception as e:
+            print(f'Bang! {e} ({current})')
+            sys.exit(1)
+
+print(f'{riddle}\nThe comments form:\n\n{comments}')
