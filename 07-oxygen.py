881656e8904de0adc61ea448072c67329d76af94
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-23 18:24:05 -0300

    Add solution to mission 07

---
+++ 07-oxygen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/def/oxygen.html
+# Source mentions oxygen.png
+
+import png  # pip install pypng
+
+from itertools import islice, takewhile
+from urllib.request import urlopen
+
+url = 'http://www.pythonchallenge.com/pc/def/oxygen.png'
+png_reader = png.Reader(urlopen(url))
+height, content = png_reader.read()[1:3]
+middle_row = next(islice(content, height // 2, height // 2 + 1))
+
+message = ''.join(chr(n) for n in takewhile(
+    lambda n: n != ord(']'), islice(middle_row, 5, None, 28)
+))
+print(f'Message is: {message}...')
+
+next_level = ''.join(chr(int(d)) for d in message.split('[', 1)[-1].split(', '))
+print(f'Next level: {next_level}')
