101d4e849f9e355900f6021d128cef82edcb95f2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-23 23:49:52 -0300

    Add solution to mission 08

---
+++ 08-integrity.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/def/integrity.html
+
+import bz2
+
+from urllib.request import urlopen
+
+url = 'http://www.pythonchallenge.com/pc/def/integrity.html'
+page_source = urlopen(url).read().decode('unicode_escape').encode('latin1')
+page_data = page_source.split(b'<!--')[1].split(b'-->')[0]
+
+_, un, _, pw, _ = page_data.split(b"'")
+
+dec_un = bz2.decompress(un)
+dec_pw = bz2.decompress(pw)
+
+print(f'user: {dec_un.decode()}')
+print(f'pass: {dec_pw.decode()}')
