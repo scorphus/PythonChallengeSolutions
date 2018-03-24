850fbdfcaff1730a9283e230c2347762ed9a08f1
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-24 19:27:01 -0300

    Add solution to mission 11

---
+++ 11-5808.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/return/5808.html
+
+from PIL import Image  # pip install pillow
+from base64 import encodebytes
+from urllib.request import Request, urlopen
+
+url = 'http://www.pythonchallenge.com/pc/return/cave.jpg'
+auth = encodebytes(b'huge:file').decode().rstrip()
+headers = {'Authorization': f'Basic {auth}'}
+
+image = Image.open(urlopen(Request(url=url, headers=headers)))
+hidden = Image.new(image.mode, [d // 2 for d in image.size])
+
+for x in range(image.width):
+    for y in range(image.height):
+        if (x + y) % 2 == 0:
+            pixel = image.getpixel((x, y))
+            hidden.putpixel((x // 2, y // 2), pixel)
+
+hidden.save('11-hidden.jpg', 'JPEG')
+print('Open 11-hidden.jpg')
