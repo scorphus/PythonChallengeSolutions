e08aab490b112b34e2cebd42ad40dc7a0924383e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-03-31 15:32:44 -0300

    Add solution to mission 15

---
+++ 15-uzi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Python Challenge Solutions
+# https://github.com/scorphus/PythonChallengeSolutions
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2018, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+# http://www.pythonchallenge.com/pc/return/uzi.html
+
+from datetime import datetime
+
+start = 1996  # Last leap year of the second millennium
+
+probable_dates = ''
+for year in range(start, 1000, -20):
+    day = f'{year}-01-27'
+    date = datetime.strptime(day, '%Y-%m-%d')
+    if date.weekday() == 1:
+        probable_dates += f'\n * {day}'
+
+print(f'Probable dates are:{probable_dates}\nCheck what happened on them!')
