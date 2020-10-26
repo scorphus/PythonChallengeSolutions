c56bfb0c9082caab1e9c0c232896a4f5f0a848df
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-10-26 21:07:51 +0100

    Use a variable name other than zip in mission 20

--- 20-idiot.py
+++ 20-idiot.py
@@ -54,6 +54,6 @@ range_start = int(msg.rstrip(".").split()[-1])
 headers["Range"] = f"bytes={range_start}-{content_max}"
 req = urlopen(Request(url=url, headers=headers))
 zip_content = req.read()
-with ZipFile(BytesIO(zip_content), "r") as zip:
-    print(f'Open {" and ".join(zip.namelist())}')
-    zip.extractall(pwd=pwd.encode())
+with ZipFile(BytesIO(zip_content), "r") as zip_file:
+    print(f'Open {" and ".join(zip_file.namelist())}')
+    zip_file.extractall(pwd=pwd.encode())
