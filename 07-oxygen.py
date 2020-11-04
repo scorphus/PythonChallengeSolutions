30934b826609933365956475fa369abdf894fb77
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-11-04 21:00:36 +0100

    Refactor solution to mission 07

--- 07-oxygen.py
+++ 07-oxygen.py
@@ -9,24 +9,23 @@
 # Copyright (c) 2018-2020, Pablo S. Blum de Aguiar <scorphus@gmail.com>
 
 # http://www.pythonchallenge.com/pc/def/oxygen.html
-# Source mentions oxygen.png
 
+from auth import get_last_src_url
+from auth import read_url
+from io import BytesIO
 from itertools import islice
 from itertools import takewhile
-from urllib.request import urlopen
 
 import png
 
 
-url = "http://www.pythonchallenge.com/pc/def/oxygen.png"
-png_reader = png.Reader(urlopen(url))
-height, content = png_reader.read()[1:3]
+url = "http://www.pythonchallenge.com/pc/def/oxygen.html"
+png_content = read_url(get_last_src_url(url))
+png_reader = png.Reader(BytesIO(png_content))
+_, height, content, *_ = png_reader.read()
 middle_row = next(islice(content, height // 2, height // 2 + 1))
 
 message = "".join(
     chr(n) for n in takewhile(lambda n: n != ord("]"), islice(middle_row, 5, None, 28))
 )
-print(f"Message is: {message}...")
-
-next_level = "".join(chr(int(d)) for d in message.split("[", 1)[-1].split(", "))
-print(f"Next level: {next_level}")
+print("".join(chr(int(d)) for d in message.split("[", 1)[-1].split(", ")))
