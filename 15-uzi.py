901546defb7cc3cf5b13fbc3ff7b723eabd8c131
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-11-06 23:17:12 +0100

    Refactor solution to mission 15

--- 15-uzi.py
+++ 15-uzi.py
@@ -13,13 +13,17 @@
 from datetime import datetime
 
 
-start = 1996  # Last leap year of the second millennium
+start = 1996  # last leap year of the second millennium
 
-probable_dates = ""
+probable_dates = []
+step = -20  # has to be a leap year ended in 6 ;-)
 for year in range(start, 1000, -20):
-    day = f"{year}-01-27"
+    day = f"{year}-01-27"  # the flowers are for the next day
     date = datetime.strptime(day, "%Y-%m-%d")
     if date.weekday() == 1:
-        probable_dates += f"\n * {day}"
+        probable_dates.append(day)
 
-print(f"Probable dates are:{probable_dates}\nCheck what happened on them!")
+print("Probable dates are:")
+print("-", "\n- ".join(probable_dates))
+print("Check what happened on them!")
+print("e.g.: https://duckduckgo.com?q=27+january+1756+wikipedia")
