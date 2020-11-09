c9a68b839997d9373bb9958297fa2b286e215be0
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-11-09 22:16:23 +0100

    Cache functions in mission 13

--- 13-disproportional.py
+++ 13-disproportional.py
@@ -11,18 +11,26 @@
 # http://www.pythonchallenge.com/pc/return/disproportional.html
 
 from auth import get_last_href_url
+from cache import autocached
 from xmlrpc.client import ServerProxy
 
 
+@autocached
 def get_evil():
     """Reads part of last mission's riddle; did you miss that???"""
     evil = __import__("12-evil")
     return next(evil.get_next_jpg_images(evil.jpg_url, (4, 5))).split()[0]
 
 
+@autocached
+def proxy_phone(proxy_url, callee):
+    with ServerProxy(proxy_url) as proxy:
+        return proxy.phone(callee)
+
+
 url = "http://www.pythonchallenge.com/pc/return/disproportional.html"
 proxy_url = get_last_href_url(url)
 
 if __name__ == "__main__":
-    with ServerProxy(proxy_url) as proxy:
-        print(proxy.phone(get_evil()))
+    callee = get_evil()
+    print(proxy_phone(proxy_url, callee))
